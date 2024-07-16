a7c055dc361b9e2c5331963a8768cf5542109afa
alvinyanson <a.yanson@arcanys.com>
2024-07-16 16:26:52 +0800

    Add project files.

---
+++ EFCoreRelationshipsTutorial/User.cs
@@ -0,0 +1,12 @@
+﻿namespace EFCoreRelationshipsTutorial
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public List<Character> Characters { get; set; }
+    }
+}
